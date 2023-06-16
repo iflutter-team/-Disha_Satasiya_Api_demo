1765d73780a6f9da37372c6cd0a50a8655b667e2
DishaSatasiya <121668639+DishaSatasiya@users.noreply.github.com>
2023-06-16 11:27:18 +0530

    done

--- lib/utils/endpoint_res.dart
+++
@@ -1,7 +0,0 @@
-class EndPointRes {
-  // base URl
-  static const baseUrl = "https://dummyjson.com/";
-  // End Point
-  static const productAPI = '${baseUrl}products';
-  static const addProductAPI = '${baseUrl}products/add';
-}
--- zip/android/app/src/main/kotlin/com/example/zip/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.zip
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
